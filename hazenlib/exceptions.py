16f6f0a601837c2c8c63556102f289554570f29a
Alex Drysdale <alexander.drysdale@wales.nhs.uk>
2025-10-17 16:08:27 +0100

    feat(exceptions): Added no distortion correct exception
    Related to #99

--- hazenlib/exceptions.py
+++ hazenlib/exceptions.py
@@ -1,4 +1,6 @@
-""" Application-specific errors"""
+"""Application-specific errors."""
+
+from __future__ import annotations
 
 # Local imports
 from hazenlib.constants import MEASUREMENT_NAMES, MEASUREMENT_TYPES
@@ -7,7 +9,8 @@ from hazenlib.constants import MEASUREMENT_NAMES, MEASUREMENT_TYPES
 class ShapeError(Exception):
     """Base exception for shapes."""
 
-    def __init__(self, shape, msg=None):
+    def __init__(self, shape: str, msg: str | None = None) -> None:
+        """Initialise the error."""
         if msg is None:
             # Default message
             msg = f"An error occured with {shape}"
@@ -16,9 +19,10 @@ class ShapeError(Exception):
 
 
 class ShapeDetectionError(ShapeError):
-    """Shape not found"""
+    """Shape not found."""
 
-    def __init__(self, shape, msg=None):
+    def __init__(self, shape: str, msg: str | None = None) -> None:
+        """Initialise the error."""
         if msg is None:
             # Default message
             msg = f"Could not find shape: {shape}"
@@ -27,12 +31,16 @@ class ShapeDetectionError(ShapeError):
 
 
 class MultipleShapesError(ShapeDetectionError):
-    """Shape not found"""
+    """Shape not found."""
 
-    def __init__(self, shape, msg=None):
+    def __init__(self, shape: tuple[str], msg: str | None = None) -> None:
+        """Initialise the error."""
         if msg is None:
             # Default message
-            msg = f"Multiple {shape}s found. Multiple shape detection is currently unsupported."
+            msg = (
+                f"Multiple {shape}s found."
+                " Multiple shape detection is currently unsupported."
+            )
 
         super(ShapeDetectionError, self).__init__(msg)
         self.shape = shape
@@ -41,7 +49,8 @@ class MultipleShapesError(ShapeDetectionError):
 class ArgumentCombinationError(Exception):
     """Argument combination not valid."""
 
-    def __init__(self, msg="Invalid combination of arguments."):
+    def __init__(self, msg: str = "Invalid combination of arguments.") -> None:
+        """Initialise the error."""
         super().__init__(msg)
 
 
@@ -66,4 +75,12 @@ class InvalidMeasurementTypeError(ValueError):
             f"Invalid measurement type: {measurement_type}."
             f" Must be one of {MEASUREMENT_TYPES}"
         )
+        super().__init__(msg)
+
+class NoDistortionCorrectionError(ValueError):
+    """No distortion correction enabled for this dataset."""
+
+    def __init__(self, name: str) -> None:
+        """Initialise the error."""
+        msg = f"Distortion correction not enabled for {name}"
         super().__init__(msg)
