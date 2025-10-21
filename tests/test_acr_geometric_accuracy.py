e8b8ba75a9031af3b31d2c442e91eea93f26d052
Alex Drysdale <alexander.drysdale@wales.nhs.uk>
2025-10-21 09:55:21 +0100

    test(tests/data/acr/): Added more ACR data

--- tests/test_acr_geometric_accuracy.py
+++ tests/test_acr_geometric_accuracy.py
@@ -1,3 +1,7 @@
+"""Test suite for the Geometric Accuracy."""
+
+# ruff: noqa: T201 S101
+
 import pathlib
 import unittest
 
@@ -9,12 +13,15 @@ from tests import TEST_DATA_DIR, TEST_REPORT_DIR
 
 
 class TestACRGeometricAccuracySiemens(unittest.TestCase):
+    """Geometric accuracy for legacy Siemens test data."""
+
     ACR_DATA = pathlib.Path(TEST_DATA_DIR / "acr" / "Siemens")
     L1 = 191.41, 187.5
     L5 = 191.41, 187.5, 191.41, 190.43
-    distortion_metrics = [-0.06, 2.5, 0.93]
+    distortion_metrics = (-0.06, 2.5, 0.93)
 
-    def setUp(self):
+    def setUp(self) -> None:
+        """Set up the test object."""
         input_files = get_dicom_files(self.ACR_DATA)
 
         self.acr_geometric_accuracy_task = ACRGeometricAccuracy(
@@ -25,7 +32,8 @@ class TestACRGeometricAccuracySiemens(unittest.TestCase):
         self.dcm_1 = self.acr_geometric_accuracy_task.ACR_obj.slice_stack[0]
         self.dcm_5 = self.acr_geometric_accuracy_task.ACR_obj.slice_stack[4]
 
-    def test_geometric_accuracy_slice_1(self):
+    def test_geometric_accuracy_slice_1(self) -> None:
+        """Test Geometric Accuracy with Slice 1."""
         slice1_vals = self.acr_geometric_accuracy_task.get_geometric_accuracy(0)
 
         slice1_vals = np.round(slice1_vals, 2)
@@ -34,11 +42,12 @@ class TestACRGeometricAccuracySiemens(unittest.TestCase):
         print("new_release:", slice1_vals)
         print("fixed value:", self.L1)
 
-        assert (slice1_vals == self.L1).all() == True
+        assert (slice1_vals == self.L1).all()
 
-    def test_geometric_accuracy_slice_5(self):
+    def test_geometric_accuracy_slice_5(self) -> None:
+        """Test Geometric Accuracy with Slice 5."""
         slice5_vals = np.array(
-            self.acr_geometric_accuracy_task.get_geometric_accuracy(4)
+            self.acr_geometric_accuracy_task.get_geometric_accuracy(4),
         )
 
         slice5_vals = np.round(slice5_vals, 2)
@@ -46,18 +55,20 @@ class TestACRGeometricAccuracySiemens(unittest.TestCase):
         print("\ntest_geo_accuracy.py::TestGeoAccuracy::test_geo_accuracy_slice5")
         print("new_release:", slice5_vals)
         print("fixed value:", self.L5)
-        assert (slice5_vals == self.L5).all() == True
+        assert (slice5_vals == self.L5).all()
 
-    def test_distortion_metrics(self):
+    def test_distortion_metrics(self) -> None:
+        """Test distortion metrics."""
         metrics = np.array(
-            self.acr_geometric_accuracy_task.get_distortion_metrics(self.L1 + self.L5)
+            self.acr_geometric_accuracy_task.get_distortion_metrics(self.L1 + self.L5),
         )
         metrics = np.round(metrics, 2)
-        assert (metrics == self.distortion_metrics).all() == True
+        assert (metrics == self.distortion_metrics).all()
 
-# TODO: Add unit tests for Philips datasets (when Philips data is available).
 class TestACRGeometricAccuracyGE(TestACRGeometricAccuracySiemens):
+    """Test Data for the GE dataset."""
+
     ACR_DATA = pathlib.Path(TEST_DATA_DIR / "acr" / "GE")
     L1 = 190.42, 188.9
     L5 = 190.42, 189.41, 190.42, 189.41
-    distortion_metrics = [-0.17, 1.1, 0.32]
+    distortion_metrics = (-0.17, 1.1, 0.32)
