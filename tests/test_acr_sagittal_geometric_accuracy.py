7608e03a18b4af7b3c9c42808d2a81a1c702581f
Alex Drysdale <alexander.drysdale@wales.nhs.uk>
2026-01-22 09:59:27 +0000

    test: enhance test_geometric_accuracy_slice_1 with better assertions and logging
    Improve the test_geometric_accuracy_slice_1 method by using assertAlmostEqual for floating point comparison and logger.info for better logging. Also, add type hint for the method. Additionally, correct class name for PhilipsAchieva test.

--- tests/test_acr_sagittal_geometric_accuracy.py
+++ tests/test_acr_sagittal_geometric_accuracy.py
@@ -33,23 +33,26 @@ class TestACRSagittalGeometricAccuracySiemens(unittest.TestCase):
         self.dcm_1 = self.acr_geometric_accuracy_task.ACR_obj.slice_stack[0]
         self.slice1_val = np.round(
             self.acr_geometric_accuracy_task.get_geometric_accuracy(
-                self.dcm_1
+                self.dcm_1,
             ),
             2,
         )
 
-    def test_geometric_accuracy_slice_1(self):
-        print(
-            "\ntest_geo_accuracy.py::TestACRSagittalGeometricAccuracy::test_geometric_accuracy_slice_1"
+    def test_geometric_accuracy_slice_1(self) -> None:
+        """Test the geometric accuracy of slice 1."""
+        logger.info(
+            "new_release: %f\n"
+            "fixed value: %f",
+            self.slice1_val,
+            self.L1,
         )
-        print("new_release:", self.slice1_val)
-        print("fixed value:", self.L1)
-
-        assert self.slice1_val == self.L1
+        self.assertAlmostEqual(self.slice1_val, self.L1, 0)
 
 
-class TestACRGeometricAccuracyPhilipsAchieva(
-    TestACRSagittalGeometricAccuracySiemens
+class TestACRSagittalGeometricAccuracyPhilipsAchieva(
+    TestACRSagittalGeometricAccuracySiemens,
 ):
+    """Base test class for the Philips Sagittal Localizer Geometric Accuracy."""
+
     ACR_DATA = pathlib.Path(TEST_DATA_DIR / "acr" / "PhilipsAchievaLocalizer")
     L1 = 146.48
